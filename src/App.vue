73e23e7e4c051012b580c352b86721ea6bc5e94c
Rosen Kunev <rkunev@gmail.com>
2017-01-21 18:14:51 +0200

    wip: Sidenav

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,65 @@
 <template>
     <div id="app">
-        <router-view></router-view>
+        <navigation-drawer></navigation-drawer>
+
+        <div class="layout">
+            <div class="header">
+                <div class="menu-icon">
+                    <md-button><img src="~assets/svg/menu.svg"></md-button>
+                </div>
+            </div>
+
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'app',
-}
+    import NavigationDrawer from 'components/NavigationDrawer';
+    import MdButton from 'components/MdButton';
+
+    export default {
+        name: 'app',
+        components: {
+            NavigationDrawer,
+            MdButton
+        },
+    }
 </script>
 
 <style lang="scss" src="assets/scss/_reset.scss"></style>
-<style>
-#app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    padding-top: 60px;
-}
+<style lang="scss">
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        padding-top: 60px;
+    }
+
+
+    .layout {
+        width: 375px;
+        height: 667px;
+        background-color: #f5f5f5;
+        position: relative;
+    }
+
+    // throw out style
+    .header {
+        background-color: #ccc;
+    }
+
+    .app-menu {
+        width: 300px;
+        height: 667px;
+        box-shadow: none;
+        background-color: #ddd;
+    }
+
+    .menu:after {
+        width: 375px;
+        height: 667px;
+    }
 </style>
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,76 @@
+<template>
+    <nav class="menu">
+        <div class="app-menu"></div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'sidenav-menu'
+    }
+</script>
+
+<style lang="scss">
+    .menu {
+        position: fixed;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        pointer-events: none;
+        z-index: 150;
+    }
+
+    .menu--visible {
+        pointer-events: auto;
+    }
+
+    .app-menu {
+        background-color: #fff;
+        color: #fff;
+        position: relative;
+        max-width: 400px;
+        width: 90%;
+        height: 100%;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.5);
+        transform: translateX(-103%);
+        display: flex;
+        flex-direction: column;
+        will-change: transform;
+        z-index: 160;
+        pointer-events: auto;
+    }
+
+    .menu--visible .app-menu {
+        transform: none;
+    }
+
+    .menu--animatable .app-menu {
+        transition: all 130ms ease-in;
+    }
+
+    .menu--visible.menu--animatable  .app-menu {
+        transition: all 330ms ease-out;
+    }
+
+    .menu:after {
+        content: '';
+        display: block;
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0,0,0,0.4);
+        opacity: 0;
+        will-change: opacity;
+        pointer-events: none;
+        transition: opacity 0.3s cubic-bezier(0,0,0.3,1);
+    }
+
+    .menu--visible.menu:after {
+        opacity: 1;
+        pointer-events: auto;
+    }
+</style>
