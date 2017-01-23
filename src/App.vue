ed0e8b9d3aee6a8441cba53818c4226c86baabfc
Rosen Kunev <rkunev@gmail.com>
2017-01-23 20:01:53 +0200

    feat(navigation): Add styling to links

--- src/App.vue
+++ src/App.vue
@@ -42,10 +42,3 @@
 </script>
 
 <style lang="scss" src="assets/scss/_reset.scss"></style>
-<style lang="scss">
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-    }
-</style>
