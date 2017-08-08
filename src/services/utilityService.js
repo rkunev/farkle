f981b5e07fa6037c4ef5306ebe0334397e5d7429
Rosen Kunev <rkunev@gmail.com>
2017-08-08 15:56:26 +0300

    feat(utility): Add nanoid for URL friendly UID generator

--- src/services/utilityService.js
+++ src/services/utilityService.js
@@ -1,10 +1,6 @@
-export function generateUniqueId(salt = _generateRandomString()) {
-    const shortSalt = salt.slice(0, 2);
-    const shortTimestamp = ((new Date()).getTime()).toString().slice(-4);
+import generate from 'nanoid/generate';
+import url from 'nanoid/url';
 
-    return btoa(shortSalt + shortTimestamp).replace(/\//g, '-');
-}
-
-function _generateRandomString(len = 4) {
-    return Math.random().toString().slice(2, 2 + len);
+export function generateShortId() {
+    return generate(url, 10);
 }
